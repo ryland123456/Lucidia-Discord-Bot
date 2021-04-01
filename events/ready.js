25817b93c847c2967ff2d93d6c96bb5da95fd408
hydrostaticcog <hydro@hydrostaticcog.org>
2021-04-01 16:44:22 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -12,8 +12,8 @@ module.exports = async (client) => {
     setInterval(() => {
         if(index === arrayOfStatus.length) index = 0;
         const status = arrayOfStatus[index];
-        client.user.setActivity('Ryland Appreciation Week!', {
-            type: "WATCHING"
+        client.user.setActivity('MARKETING SCAMS', {
+            type: "LISTENING"
         });
         index++;
     }, 20000)}
